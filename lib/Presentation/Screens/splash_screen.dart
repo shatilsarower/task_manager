2df723445857d4458578d4452598461a49ad5843
Sarwar Murshed Shatil <38027343+shatilsarower@users.noreply.github.com>
2024-03-19 00:58:13 +0600

    Complementing the flow of Splash screen.

--- lib/Presentation/Screens/splash_screen.dart
+++ lib/Presentation/Screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:task_managment/Presentation/Screens/sign_screen.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -8,21 +10,41 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    _moveToSignIn();
+  }
+
+
+  Future<void> _moveToSignIn() async {
+    await Future.delayed(const Duration(seconds: 2));
+    if (mounted) {
+      Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (context) => const SignInScreen(),),);
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Stack(
-         children: [
-           SvgPicture.asset('assets/images/background.svg',height: double.infinity, width: double.infinity,
-             fit: BoxFit.cover,
-           ),
-           Center(
-             child: SvgPicture.asset('assets/images/logo.svg',
-               width: 120,
-               fit: BoxFit.scaleDown,
-             ),
-           ),
-         ],
+        children: [
+          SvgPicture.asset(
+            'assets/images/background.svg', height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: SvgPicture.asset('assets/images/logo.svg',
+              width: 120,
+              fit: BoxFit.scaleDown,
+            ),
+          ),
+        ],
       ),
     );
   }
